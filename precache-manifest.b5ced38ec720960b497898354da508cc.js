c876cda053fb3928383c66fedd52d09d56b1e152
Jirka Chadima <chadima.jiri@gmail.com>
2019-07-31 15:45:40 +0200

    Updates

---
+++ precache-manifest.b5ced38ec720960b497898354da508cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4adbe10fa0162e800e7e87f02e4cf32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d5c9c8d54d134f4dd16",
+    "url": "/static/css/2.df1e4690.chunk.css"
+  },
+  {
+    "revision": "8a8598a4fc49c251e8b6",
+    "url": "/static/css/main.f6f1ab4f.chunk.css"
+  },
+  {
+    "revision": "4d5c9c8d54d134f4dd16",
+    "url": "/static/js/2.8f946325.chunk.js"
+  },
+  {
+    "revision": "8a8598a4fc49c251e8b6",
+    "url": "/static/js/main.10de953a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
